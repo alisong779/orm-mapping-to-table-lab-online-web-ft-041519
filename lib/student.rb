b5bf72e66e8c04ac04c61770090188ab65617883
Alison Golden <alisong779@yahoo.com>
2019-05-22 02:08:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
   sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
-    SQL
+      SQL
     DB[:conn].execute(sql, self.name, self.grade)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
@@ -36,5 +36,7 @@ class Student
   end
   
   def self.drop_table
+    sql = <<-SQL
+    
   end
 end
